e5077bd1ac27d95a4c6f69c52c7c65fdb1467089
Samuel Fabrizio <samuelfabrizio@gmail.com>
2017-06-05 01:22:26 +0200

    [ADD] basic unit test

---
+++ test/index.spec.js
@@ -0,0 +1,7 @@
+var indexFile = require('../index');
+
+describe('index', function() {
+    it('should return an object', function() {
+        expect(typeof indexFile).toBe('object');
+    });
+});
---
+++ test/main.spec.js
@@ -0,0 +1,12 @@
+var main = require('../lib/main');
+
+describe('main', function() {
+    it('should return an object', function() {
+        expect(typeof main).toBe('object');
+    });
+
+    it('should have print property', function() {
+        expect(main.print).toBeDefined();
+    });
+
+});
